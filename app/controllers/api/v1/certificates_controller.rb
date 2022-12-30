4fca82c9516db7fdb487b178c1304d0d147f5c07
shakeer.faleeldeen <shakeer.faleeldeen@ooma.com>
2022-12-31 00:31:15 +0530

    added test cases for expire date api call

--- app/controllers/api/v1/certificates_controller.rb
+++ app/controllers/api/v1/certificates_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::CertificatesController < ApplicationController
     x509 = OpenSSL::X509::Certificate.new(params[:cert].to_s)
     render json: { expire_date: x509.not_after, is_expired: Time.now > x509.not_after, status: 200 }
   rescue OpenSSL::X509::CertificateError
-    render json: { message: "Invalid Certificate Input", status: 400 }
+    render json: { message: "Invalid Certificate Input" }, status: :unprocessable_entity
   end
 end
--- test/controllers/api/v1/certificates_controller_test.rb
+++ test/controllers/api/v1/certificates_controller_test.rb
@@ -1,18 +1,36 @@
 require "test_helper"
 
 class Api::V1::CertificatesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_certificates_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get api_v1_certificates_show_url
-    assert_response :success
-  end
+  include SelfSignedCertificateGenerator
 
   test "should get expire_date" do
-    get api_v1_certificates_expire_date_url
+    get api_v1_certificates_expire_date_url(cert: generate_certificate )
     assert_response :success
+  end
+
+  test "should return invalid message for errored certificate input" do
+    get api_v1_certificates_expire_date_url(cert: "")
+    resp = JSON.parse @response.body
+    assert_equal 'Invalid Certificate Input', resp['message']
+  end
+
+  test "should return is_expired message as true for expired certificate" do
+    options = {
+      expire_in_days: -1
+    }
+
+    get api_v1_certificates_expire_date_url(cert: generate_certificate(options))
+    resp = JSON.parse @response.body
+    assert_equal true, resp['is_expired']
+  end
+
+  test "should return is_expired message as false for non expired certificate" do
+    options = {
+      expire_in_days: 1
+    }
+
+    get api_v1_certificates_expire_date_url(cert: generate_certificate(options))
+    resp = JSON.parse @response.body
+    assert_equal false, resp['is_expired']
   end
 end
